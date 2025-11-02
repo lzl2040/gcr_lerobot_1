aac581dac531336ca2bab6f669b3fb5b8f836b32
lzlglory <lizuolei24@ime.ac.cn>
2025-11-02 00:19:55 +0000

    update

--- lerobot/scripts/dps_train.py
+++ lerobot/scripts/dps_train.py
@@ -266,6 +266,8 @@ def train(cfg: TrainPipelineConfig):
                             prefetch_factor=4
                             )
     # DeepSpeed initialization
+    if cfg.policy.type == "pi05":
+        policy.resize_token_embedding()
     model_engine, optimizer, _, lr_scheduler = deepspeed.initialize(
         model=policy,
         optimizer=optimizer,
