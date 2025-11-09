9204140d9e667d0db97db0dffcffb1a6a1ad1e42
lzlglory <lizuolei24@ime.ac.cn>
2025-11-09 11:28:12 +0000

    update with new token and wandb

--- finetune_on_40G.sh
+++ finetune_on_40G.sh
@@ -60,8 +60,8 @@ python -m lerobot.scripts.dps_train \
     --dataset.data_mix=$DATA_MIX \
     --dataset.use_state=$USE_STATE \
     --dataset.image_transforms.enable=true \
-    --wandb.enable=false \
-    --resume=true \
+    --wandb.enable=true \
+    --resume=false \
     --wandb.project="pi05-ft-simulated" \
     --job_name=$JOB_NAME \
     --log_dir="/mnt/wangxiaofa/logs" \
--- lerobot/scripts/dps_train.py
+++ lerobot/scripts/dps_train.py
@@ -459,5 +459,5 @@ def train(cfg: TrainPipelineConfig):
 if __name__ == "__main__":
     # os.environ["TOKENIZERS_PARALLELISM"] = "false"
     # https://wandb.ai/authorize
-    # os.environ['WANDB_API_KEY'] = '9e1c3ac77856b8ebb5573c4e1e250c84aabfb904'
+    os.environ['WANDB_API_KEY'] = '9e1c3ac77856b8ebb5573c4e1e250c84aabfb904'
     train()
