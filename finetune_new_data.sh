8c2b66d5afda1a709e388e6cfe7488db22eda6d0
lzlglory <lizuolei24@ime.ac.cn>
2025-11-09 08:09:50 +0000

    update

--- finetune_new_data.sh
+++ finetune_new_data.sh
@@ -60,7 +60,7 @@ python -m lerobot.scripts.dps_train_2 \
     --dataset.data_mix=$DATA_MIX \
     --dataset.use_state=$USE_STATE \
     --dataset.image_transforms.enable=false \
-    --wandb.enable=false \
+    --wandb.enable=true \
     --resume=false \
     --wandb.project="pi0-ft-simulated" \
     --job_name=$JOB_NAME \
