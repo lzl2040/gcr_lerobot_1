21623f1a30f29da93c4f6f73f2b5ec8acaad4f61
lzlglory <lizuolei24@ime.ac.cn>
2025-11-10 16:32:13 +0000

    update wrist_aug

--- lerobot/common/datasets/transforms.py
+++ lerobot/common/datasets/transforms.py
@@ -204,11 +204,11 @@ class ImageTransformsConfig:
                 type="SharpnessJitter",
                 kwargs={"sharpness": (0.5, 1.5)},
             ),
-            # "crop_resize":ImageTransformConfig(
-            #     weight=1.0,
-            #     type="RandomResizedCrop",
-            #     kwargs={"size": (256, 256), "scale" : (0.9, 0.95), "ratio": (1.0, 1.0)},
-            # ),
+            "crop_resize":ImageTransformConfig(
+                weight=1.0,
+                type="RandomResizedCrop",
+                kwargs={"size": (256, 256), "scale" : (0.9, 0.95), "ratio": (1.0, 1.0)},
+            ),
             # "rotate": ImageTransformConfig(
             #     weight=1.0,
             #     type="RandomRotate",
